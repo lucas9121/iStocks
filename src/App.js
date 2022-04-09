c01a82b512ace51a4e3dfe3a8660a1cf87b3881c
Lucas <lucas2carlos@gmail.com>
2022-04-09 12:58:34 -0400

    building App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Route, Routes} from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+
+
+export default function App(){
+    return (
+       <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />}/>
+            </Routes>
+       </>
+    )
+}
