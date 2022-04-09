4921e90fa3de83df6c93027bd7d987dc7c8b80d9
Lucas <lucas2carlos@gmail.com>
2022-04-09 13:43:13 -0400

    building Stock.js

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,26 +1,31 @@
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react"
+import {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import stocks from '../data'
 
-export default function Stock(props) {
+export default function Stock() {
     const params = useParams()
     const symbol = params.symbol
-    const [stock, setStock] = useState(null)
-    const getStock = async () =>{
-        try{
-            const response = stock.find((stock)=>{
-                stock.symbol === symbol
-               setStock(symbol) 
-            }); 
-            
-        }
-        catch (e){
-            console.error(e)
+
+    const [stock, setStock] = useState({})
+
+    const getStock = async () => {
+        try {
+            const data = await stocks.find(s => s.symbol === symbol)
+            setStock(data)
+        } catch (err) {
+            console.log(err)
         }
     }
-    return(
-        <div>
-            <h1>Some Stock</h1>
-        </div>
-    )
 
+    useEffect(() => {
+        getStock()
+    }, [])
+
+    return (
+        <main>
+            <h1>Stock Page</h1>
+            <p>{stock.name}</p>
+        </main>
+        
+    )
 }
