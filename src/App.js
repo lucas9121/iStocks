b71dcfe668e7ff1ddacf4bb365ba6b1f88415ba9
Lucas <lucas2carlos@gmail.com>
2022-04-09 13:37:50 -0400

    building App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import {Route, Routes} from "react-router-dom"
 import Header from "./components/Header"
+import Nav from "./components/Nav"
 import Home from "./pages/Home"
+import { Route, Routes } from "react-router-dom"
+import About from "./pages/About"
+import Stocks from "./pages/Dashboard"
+import Stock from "./pages/Stock"
 
-
-export default function App(){
+export default function App() {
     return (
-       <>
+        <div>
             <Header />
+            <Nav />
             <Routes>
-                <Route path="/" element={<Home />}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/stocks" element={<Stocks />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/stocks/:symbol" element={<Stock />} />
             </Routes>
-       </>
+
+        </div>
     )
 }
