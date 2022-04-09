0cd6bad2d2aeec1b9fec2197590831f68e545f59
Lucas <lucas2carlos@gmail.com>
2022-04-09 16:07:28 -0400

    building Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom"
 import stocks from "../data"
+import { useState, useEffect } from "react";
 
 
 export default function Stocks() {
+    const apiKey = "1693ef68995ff34e46bfa87d2b6b909f"
+    // const URL = `https://financialmodelingprep.com/api/v3/quote/${stockList}?apikey=${apiKey}`;
+    const [stocksData, setStocksData] = useState([]);
+    const stockList = stocks.map((s => s.symbol)).join(',')
     return (
         <main>
             <h1>Stocks</h1>
