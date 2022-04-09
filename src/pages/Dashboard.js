9c11e08686bcc900cac56f16321fc1f11afcc838
Lucas <lucas2carlos@gmail.com>
2022-04-09 13:12:20 -0400

    building Dashboard

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import stocks from "../data"
+
+
+export default function Stocks() {
+    return (
+        <div>
+            <h1>Stocks</h1>
+            <ul>
+                {
+                    stocks.map((stock, idx) => {
+                        const { name, symbol } = stock;
+                        return (
+
+                            <li key={idx}><Link to={`/stocks/${symbol}`}>{name}</Link></li>
+
+                        )
+                    })
+                }
+            </ul>
+        </div>
+
+    )
+}
