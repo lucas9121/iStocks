f36bc55236151a38869505d5c6a64e70ff960332
Lucas <lucas2carlos@gmail.com>
2022-04-11 23:34:59 -0400

    building About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,19 @@
 export default function About() {
     return (
         <main>
-            <h1>About Page</h1>
-            <p>Team effort to build this awesome page</p>
+            <h1>SEIR-Madeline</h1>
+            <h2>Teo's Team</h2>
+            <ol>
+                <li>Lucas C.</li>
+                <li>Charlie F.</li>
+                <li>Dan N.</li>
+                <li>Farah Y.</li>
+                <li>Gerimae O.</li>
+                <li>Julio C.</li>
+                <li>Mikey D.</li>
+                <li>Ray K.</li>
+            </ol>
+            <p>This project began in class during breakout groups, and most of the features were made as a group effort.</p>
         </main>
     )
 }
