f5729ab908ffeafb720ed5600a7695552c1e4ae0
Lucas <lucas2carlos@gmail.com>
2022-04-13 00:44:19 -0400

    building Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -63,8 +63,8 @@ export default function Stocks() {
                                 return(
                                     <tr key={idx}>
                                         <td className="td"><Link to={`/stocks/${stock.symbol}`}>{stock.name} ({stock.symbol})</Link></td>
-                                        <td className="td"><small>$</small>{stock.price}</td>
-                                        {stock.change > 0 ? <td className="td"><span style={{color: 'green'}}><small>$</small>{stock.change.toFixed(2)} (+{stock.changesPercentage.toFixed(2)}<small>%</small>)</span></td> : <td className="td"><span style={{color: 'red'}}><small>$</small>{stock.change.toFixed(2)} ({stock.changesPercentage.toFixed(2)}<small>%</small>)</span></td>}
+                                        <td className="td"><Link to={`/stocks/${stock.symbol}`}><small>$</small>{stock.price}</Link></td>
+                                        {stock.change > 0 ? <td className="td"><Link to={`/stocks/${stock.symbol}`}><span style={{color: 'green'}}><small>$</small>{stock.change.toFixed(2)} (+{stock.changesPercentage.toFixed(2)}<small>%</small>)</span></Link></td> : <td className="td"><Link to={`/stocks/${stock.symbol}`}><span style={{color: 'red'}}><small>$</small>{stock.change.toFixed(2)} ({stock.changesPercentage.toFixed(2)}<small>%</small>)</span></Link></td>}
                                     </tr>
                                 )
                             })
