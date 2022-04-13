589c2c26bb2e4a17a8f408ff7a6f42dbba296076
Lucas <lucas2carlos@gmail.com>
2022-04-13 01:23:26 -0400

    building Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,11 @@
 import { Link } from "react-router-dom"
-// import stocks from "../data"
 import { useState, useEffect } from "react";
 
 
 export default function Stocks() {
     const apiKey = "1693ef68995ff34e46bfa87d2b6b909f"
-    // const URL = `https://financialmodelingprep.com/api/v3/quote/${stockList}?apikey=${apiKey}`;
     const URL = `https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=${apiKey}`
     const [stocks, setStocks] = useState([]);
-    // const stockList = stocks.map((s => s.symbol)).join(',')
     const [color, setColor] = useState('black')
     const [one, setOne] = useState(0)
     
@@ -29,22 +26,6 @@ export default function Stocks() {
         getStock()
     }, []);
 
-    // return (
-    //     <main>
-    //         <h1>Stocks</h1>
-    //         <ul>
-    //             {
-    //                 stocks.map((stock, idx) => {
-    //                     const { symbol } = stock;
-    //                     return (
-    //                         <li key={idx}><Link to={`/stocks/${symbol}`}>{symbol}</Link></li>
-    //                     )
-    //                 })
-    //             }
-    //         </ul>
-    //     </main>
-
-    // )
     const loaded = () => {
         return(
             <main>
@@ -65,9 +46,6 @@ export default function Stocks() {
                                         <td className="td"><Link to={`/stocks/${stock.symbol}`}>{stock.name} ({stock.symbol})</Link></td>
                                         <td className="td"><small>$</small>{stock.price}</td>
                                         {stock.change > 0 ? <td className="td"><span style={{color: 'green'}}><small>$</small>{stock.change.toFixed(2)} (+{stock.changesPercentage.toFixed(2)}<small>%</small>)</span></td> : <td className="td"><span style={{color: 'red'}}><small>$</small>{stock.change.toFixed(2)} ({stock.changesPercentage.toFixed(2)}<small>%</small>)</span></td>}
-                                        {/* <td className="td"><Link to={`/stocks/${stock.symbol}`}>{stock.name} ({stock.symbol})</Link></td>
-                                        <td className="td"><Link to={`/stocks/${stock.symbol}`}><small>$</small>{stock.price}</Link></td>
-                                        {stock.change > 0 ? <td className="td"><Link to={`/stocks/${stock.symbol}`}><span style={{color: 'green'}}><small>$</small>{stock.change.toFixed(2)} (+{stock.changesPercentage.toFixed(2)}<small>%</small>)</span></Link></td> : <td className="td"><Link to={`/stocks/${stock.symbol}`}><span style={{color: 'red'}}><small>$</small>{stock.change.toFixed(2)} ({stock.changesPercentage.toFixed(2)}<small>%</small>)</span></Link></td>} */}
                                     </tr>
                                 )
                             })
